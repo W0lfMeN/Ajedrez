2d3c1604075cd4d0185f19e3af4756ddaf04943e
Carlos <Carlos@Carlos-HP>
2018-11-21 20:50:18 +0100

    se ha creado los metodos get y set para la clase posicion

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -9,7 +9,35 @@ package org.iesalandalus.programacion.caballoajedrez;
  *
  * @author Carlos
  */
+
+//Creamos la clase posicion con sus atributos
 public class Posicion {
-    int fila;
-    char columna;
+    private int fila;
+    private char columna;
+    
+    
+    /*creamos los metodos get y set y si para el set la fila no es del 1 al 8 
+    ni la columna es desde la a hasta la h muestre un illegalArgumentException*/
+    
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila>=1 || fila<=8)
+            this.fila = fila;
+        else
+            throw new IllegalArgumentException("Error, se ha introducido una fila incorrecta");
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if(columna!='a' & columna!='b' & columna!='c' & columna!='d' & columna!='e' & columna!='f' & columna!='g' & columna!='h')
+            throw new IllegalArgumentException("Error, se ha introducido una columna incorrecta");
+        else
+            this.columna = columna;
+    }
 }
